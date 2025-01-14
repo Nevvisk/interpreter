138f61c54b8f8beec770c06bf4b0ac9114319dff
nevvisk <r.simmerling@gmx.de>
2025-01-14 22:33:00 +0100

    testing nvim plugins and the open change was annoying me

--- src/main.go
+++ src/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-    "fmt"
-    "interpreter/src/repl"
-    "os"
-    "os/user"
-) 
+	"fmt"
+	"interpreter/src/repl"
+	"os"
+	"os/user"
+)
 
-func main(){
-    user, err := user.Current()
-    if err != nil {
-        panic(err)
-    }
-    fmt.Printf("Hello %s this is the interpreter.", user.Username)
-    fmt.Printf("Type in commands\n")
-    repl.Start(os.Stdin, os.Stdout)
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s this is the interpreter.", user.Username)
+	fmt.Printf("Type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
