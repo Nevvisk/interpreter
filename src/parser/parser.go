883dcd12077415dd21de88762433a7ef63d0122b
nevvisk <r.simmerling@gmx.de>
2025-02-03 21:21:30 +0100

    added Integer parsing

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"interpreter/src/ast"
 	"interpreter/src/lexer"
 	"interpreter/src/token"
+	"strconv"
 )
 
 type Parser struct {
@@ -42,6 +43,7 @@ func New(l *lexer.Lexer) *Parser {
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 
 	return p
 }
@@ -86,7 +88,7 @@ func (p *Parser) parseStatement() ast.Statement {
 	case token.RETURN:
 		return p.parseReturnStatement()
 	default:
-		return parseExpressionStatement()
+		return p.parseExpressionStatement()
 	}
 }
 
@@ -167,4 +169,17 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 	}
 	leftExp := prefix()
 	return leftExp
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	lit := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+	lit.Value = value
+	return lit
 }
